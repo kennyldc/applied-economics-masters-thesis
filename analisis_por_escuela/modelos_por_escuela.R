927d6431ed59fe0dba7f8dbbf3a717d3b721c426
kennyldc <caribaz@gmail.com>
2022-05-21 18:42:48 -0500

    getting the summary dataframes with a more elegant code syntax

--- analisis_por_escuela/modelos_por_escuela.R
+++ analisis_por_escuela/modelos_por_escuela.R
@@ -57,9 +57,15 @@ interacciones_financiamiento <- unlist(lapply(combinaciones_incidentes, \(x) pas
 # getting the right hand coefficient terms for interaction term with school shift (morning or afternoon)
 interacciones_turno <- unlist(lapply(combinaciones_incidentes, \(x) paste0(x, " + " , x,"*","N_TURNO_BASE")))
 
+# merging all combinations of right hand coef terms into a single list
+terminos_coef_posibles <- list(incidentes = combinaciones_incidentes,
+                               incidentes_interaccion_carencias = interacciones_carencias,
+                               incidentes_interaccion_financiamiento = interacciones_financiamiento,
+                               incidentes_interaccion_turno = interacciones_turno)
+
 # LOG-LOG transformations -------------------------------------------------
 # for the variables that reflect the number of crimes around the school there are several with a value equal to 0.
-# Even for the mean grades, the minimun value in each case is 0
+# Even for the mean grades, the minimum value in each case is 0
 # summary(nuevo_panel[17:54])
 
 # I follow the common practice to add a value of 1 in order to make the log-log transformation.
@@ -67,92 +73,58 @@ interacciones_turno <- unlist(lapply(combinaciones_incidentes, \(x) paste0(x, " 
 nuevo_panel_log_log <- nuevo_panel
 nuevo_panel_log_log[17:54] <- log(nuevo_panel_log_log[17:54]+1)
 
-
 # Estimates
 # MEAN_CALIF_ESP -----------------------------------------
 
-# Running the regressions for the data without any transformation and NULL weights for all combination of crimes
-resumen_coeficientes_esp <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos=NULL)))
-# Running the regressions for the data without any transformation and WITH weights. Weights are pondered by the number of alumni in that school in terms of others from the same level.
-resumen_coeficientes_esp_weights <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos="YES")))
+# Making each regression (without weights) for all combinations of righ hand coefficient terms (including interactions)
+# The results are stored in a list (of dataframes) with the summaries of the estimate for each coefficient, the type of crime, 
+# the number of days and the distance, the standard deviation, p value and adj R2
+# To make the code less obscure, the first lapply iterates the making of the dataframe for each right hand coefficient term 
+# specified in the terminos_coef_posibles list (that is why the output has 4 daframes) with the use of an anonymous function,
+# (the syntax \(x) is a new feature from R 4.0)
+# The map_df produces the dataframe by iterating each coefficient from the combinations made above (see line 52 to 58) into
+# the reg_felm function that produces a summary specyfing the data and if the weights (pesos) are required.
+estimaciones_esp_sin_pesos <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos=NULL))
+})
 
-# adding interaction term with social underdevelopment index, null weights
-resumen_coeficientes_esp_carencias <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with social underdevelopment index, with weights
-resumen_coeficientes_esp_carencias_weights <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos="YES")))
+# Now repeating the process of running all regressions but for regressions that INCLUDE weights.
+# Weights ponder the number of alumni in that school in terms of other schools from the same level.
+estimaciones_esp_pesos <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos="YES"))
+})
 
-# adding interaction term with school funding, null weights. The omitted variable is private funding
-resumen_coeficientes_esp_financiamiento <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with school funding, with weights. The omitted variable is private funding
-resumen_coeficientes_esp_financiamiento_weights <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos="YES")))
+# MEAN_CALIF_ESP LOG-LOG Transformation -----------------------------------------
+# Because the interpretation of the coefficient estimates is more or less difficult AND because the number of crimes are close to 0,
+# I decide to run the same regressions but making a log transformation for both y and x 
 
-# adding interaction term with school shift, null weights. The omitted variable is morning shift
-resumen_coeficientes_esp_turno <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with school shift, with weights. The omitted variable is morning shift
-resumen_coeficientes_esp_turno_weights <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel, pesos="YES")))
+estimaciones_esp_sin_pesos_log_log <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL))
+})
 
-# MEAN_CALIF_ESP LOG-LOG -----------------------------------------
-#Same estimations as above but with the log-log specification
-
-# Running the regressions for the data with log log  transformation and NULL weights for all combination of crimes
-resumen_coeficientes_esp_log_log <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL)))
-# Running the regressions for the data with log log transformation and WITH weights. Weights are pondered by the number of alumni in that school in terms of others from the same level.
-resumen_coeficientes_esp_log_log_weights <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with social underdevelopment index, null weights
-resumen_coeficientes_esp_log_log_carencias <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with social underdevelopment index, with weights
-resumen_coeficientes_esp_carencias_log_log_weights <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with school funding, null weights. The omitted variable is private funding
-resumen_coeficientes_esp_log_log_financiamiento <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with school funding, with weights. The omitted variable is private funding
-resumen_coeficientes_esp_financiamiento_log_log_weights <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with school shift, null weights. The omitted variable is morning shift
-resumen_coeficientes_esp_log_log_turno <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with school shift, with weights. The omitted variable is morning shift
-resumen_coeficientes_esp_turno_log_log_weights <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos="YES")))
+estimaciones_esp_pesos_log_log <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_ESP", x, nuevo_panel_log_log, pesos=NULL))
+})
 
 # MEAN_CALIF_MAT -----------------------------------------
-# same procedure as spanish WITHOUT transformation and without weights
-resumen_coeficientes_mat <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos=NULL)))
-# same procedure as spanish  WITHOUT transformation but WITH weights
-resumen_coeficientes_mat_weights <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos="YES")))
+# Same procedures as in the spanish section
+# regression without tranforming the data and null weights
+estimaciones_mat_sin_pesos <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos=NULL))
+})
 
-# adding interaction term with social underdevelopment index, null weights
-resumen_coeficientes_mat_carencias <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with social underdevelopment index, with weights
-resumen_coeficientes_mat_carencias_weights <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos="YES")))
+# MATH regressions with weights
+estimaciones_mat_pesos <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos="YES"))
+})
 
-# adding interaction term with school funding, null weights. The omitted variable is private funding
-resumen_coeficientes_mat_financiamiento <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with school funding, with weights. The omitted variable is private funding
-resumen_coeficientes_mat_financiamiento_weights <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos="YES")))
+# MEAN_CALIF_MAT LOG-LOG Transformation -----------------------------------------
+# I decide to run the same regressions but making a log transformation for both y and x 
 
-# adding interaction term with school shift, null weights. The omitted variable is morning shift
-resumen_coeficientes_mat_turno <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos=NULL)))
-# adding interaction term with school shift, with weights. The omitted variable is morning shift
-resumen_coeficientes_mat_turno_weights <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel, pesos="YES")))
+estimaciones_mat_sin_pesos_log_log <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL))
+})
 
-# MEAN_CALIF_MAT LOG-LOG -----------------------------------------
-
-# Running the regressions for the data with log log  transformation and NULL weights for all combination of crimes
-resumen_coeficientes_mat_log_log <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL)))
-# Running the regressions for the data with log log transformation and WITH weights. Weights are pondered by the number of alumni in that school in terms of others from the same level.
-resumen_coeficientes_mat_log_log_weights <- bind_rows(lapply(combinaciones_incidentes, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with social underdevelopment index, null weights
-resumen_coeficientes_mat_log_log_carencias <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with social underdevelopment index, with weights
-resumen_coeficientes_mat_carencias_log_log_weights <- bind_rows(lapply(interacciones_carencias, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with school funding, null weights. The omitted variable is private funding
-resumen_coeficientes_mat_log_log_financiamiento <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with school funding, with weights. The omitted variable is private funding
-resumen_coeficientes_mat_financiamiento_log_log_weights <- bind_rows(lapply(interacciones_financiamiento, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos="YES")))
-
-# adding interaction term with school shift, null weights. The omitted variable is morning shift
-resumen_coeficientes_mat_log_log_turno <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL)))
-# adding interaction term with school shift, with weights. The omitted variable is morning shift
-resumen_coeficientes_mat_turno_log_log_weights <- bind_rows(lapply(interacciones_turno, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos="YES")))
+estimaciones_mat_pesos_log_log <- lapply(terminos_coef_posibles, \(x){
+  map_df(x, \(x) reg_felm("MEAN_CALIF_MAT", x, nuevo_panel_log_log, pesos=NULL))
+})
